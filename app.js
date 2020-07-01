299dc8fb16b10d1b908bea2caae8335ea7ed6106
Ridmod <hridaymodi@hotmail.com>
2020-07-01 17:18:53 +0530

    added forgot password

--- app.js
+++ app.js
@@ -220,7 +220,141 @@ app.get("/logout", function (req, res) {
 app.get("/forgot", function (req, res) {
   res.render("forgot");
 });
+app.post("/forgot", function (req, res, next) {
+  async.waterfall(
+    [
+      function (done) {
+        crypto.randomBytes(20, function (err, buf) {
+          var token = buf.toString("hex");
+          done(err, token);
+        });
+      },
+      function (token, done) {
+        User.findOne({ username: req.body.username }, function (err, user) {
+          if (!user) {
+            return res.redirect("/forgot");
+          }
 
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+          user.save(function (err) {
+            done(err, token, user);
+          });
+        });
+      },
+      function (token, user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "eunoia.cestlavie@gmail.com",
+            pass: "eunoia123",
+          },
+        });
+        var mailOptions = {
+          to: user.username,
+          from: "eunoia.cestlavie@gmail.com",
+          subject: " Password Reset",
+          text:
+            "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+            "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+            "http://" +
+            req.headers.host +
+            "/reset/" +
+            token +
+            "\n\n" +
+            "If you did not request this, please ignore this email and your password will remain unchanged.\n",
+        };
+        smtpTransport.sendMail(mailOptions, function (err) {
+          console.log("mail sent");
+          done(err, "done");
+        });
+      },
+    ],
+    function (err) {
+      if (err) return next(err);
+      res.redirect("/forgot");
+    }
+  );
+});
+
+//RESET
+app.get("/reset/:token", function (req, res) {
+  User.findOne(
+    {
+      resetPasswordToken: req.params.token,
+      resetPasswordExpires: { $gt: Date.now() },
+    },
+    function (err, user) {
+      if (!user) {
+        console.log("here");
+        return res.redirect("/forgot");
+      }
+      res.render("reset", { token: req.params.token });
+    }
+  );
+});
+
+app.post("/reset/:token", function (req, res) {
+  async.waterfall(
+    [
+      function (done) {
+        User.findOne(
+          {
+            resetPasswordToken: req.params.token,
+            resetPasswordExpires: { $gt: Date.now() },
+          },
+          function (err, user) {
+            if (!user) {
+              return res.redirect("back");
+            }
+            if (req.body.password === req.body.confirm) {
+              user.setPassword(req.body.password, function (err) {
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+
+                user.save(function (err) {
+                  req.logIn(user, function (err) {
+                    done(err, user);
+                  });
+                });
+              });
+            } else {
+              return res.redirect("back");
+            }
+          }
+        );
+      },
+      function (user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "eunoia.cestlavie@gmail.com",
+            pass: "eunoia123",
+          },
+        });
+        var mailOptions = {
+          to: user.username,
+          from: "eunoia.cestlavie@gmail.com",
+          subject: "Your password has been changed",
+          text:
+            "Hello,\n\n" +
+            "This is a confirmation that the password for your account " +
+            user.username +
+            " has just been changed.\n",
+        };
+        smtpTransport.sendMail(mailOptions, function (err) {
+          done(err);
+        });
+      },
+    ],
+    function (err) {
+      res.redirect("/events");
+    }
+  );
+});
+
+//MIDDLEWARE2
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
--- models/user.js
+++ models/user.js
@@ -1,12 +1,13 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
-  username: String,
+  username: { type: String, unique: true, required: true },
   password: String,
   firstname: String,
   lastname: String,
   phone: Number,
-  email: { type: String, unique: true, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
 });
 UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
